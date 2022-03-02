b9a3445f59fbe6605c3b18293c2e8c71435c6944
Shiv Bhonde <shivbhonde04@gmail.com>
2022-03-02 09:23:46 +0530

    add google analytics to next-site

--- next-site/pages/_app.js
+++ next-site/pages/_app.js
@@ -1,13 +1,47 @@
 import '../styles/globals.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Script from 'next/script';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as ga from '../lib/googleAnalytics';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
 	useEffect(() => {
 		AOS.init({});
 	}, []);
-	return <Component {...pageProps} />;
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			ga.pageview(url);
+		};
+
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
+	return (
+		<>
+			<Script
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics-script" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+				  
+					gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+				`}
+			</Script>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
